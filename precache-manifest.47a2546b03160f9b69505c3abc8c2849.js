909be5da0b513d64f54be15e5c81d71fed230191
Michael Molina <mikey.molina@xircus.app>
2021-08-10 13:47:52 +0800

    Updates

---
+++ precache-manifest.47a2546b03160f9b69505c3abc8c2849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec7cfb10806fe07fef80dd451a0afe9",
+    "url": "/use-xircus/index.html"
+  },
+  {
+    "revision": "9afd226a78feb113086f",
+    "url": "/use-xircus/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "f4f45f04847f05d8ef85",
+    "url": "/use-xircus/static/js/2.39bdb4e1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-xircus/static/js/2.39bdb4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afd226a78feb113086f",
+    "url": "/use-xircus/static/js/main.f4517d45.chunk.js"
+  },
+  {
+    "revision": "8e2c4ed5f932a22687ad",
+    "url": "/use-xircus/static/js/runtime-main.0f784ed4.js"
+  }
+]);
